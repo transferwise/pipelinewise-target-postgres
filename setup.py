0562081267195815f3c7c4adf1aa99987e39f28b
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-08-12 12:09:54 +0100

    Bump version and add classifiers

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@
 
 from setuptools import setup
 
+with open('README.md') as f:
+      long_description = f.read()
+
 setup(name="pipelinewise-target-postgres",
-      version="1.0.0",
+      version="1.0.1",
       description="Singer.io target for loading data to PostgreSQL - PipelineWise compatible",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author="TransferWise",
       url='https://github.com/transferwise/pipelinewise-target-postgres',
-      classifiers=["Programming Language :: Python :: 3 :: Only"],
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
       py_modules=["target_postgres"],
       install_requires=[
           'singer-python==5.1.1',
